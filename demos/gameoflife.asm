6270af66ade9754ebd4aba6bf0530ec196934b63
Rosie <acquitefx@gmail.com>
2025-05-07 13:06:16 +0100

    refactor: less instructions per neighbour

--- demos/gameoflife.asm
+++ demos/gameoflife.asm
@@ -11,98 +11,50 @@ cycle:
   mov %r1, 0
   game_loop:
     add %r4, %r1, 0xbf7f
-    cmp %r4, 0xc000
-    jge out1
-    add %r4, %r4, 0x3000
-  out1:
-    cmp %r4, 0xefff
-    jle out2
-    sub %r4, %r4, 0x3000
-  out2:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; up left
     add %r2, %r0, %r3
 
     add %r4, %r1, 0xbf80
-    cmp %r4, 0xc000
-    jge out3
-    add %r4, %r4, 0x3000
-  out3:
-    cmp %r4, 0xefff
-    jle out4
-    sub %r4, %r4, 0x3000
-  out4:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; up
     add %r2, %r2, %r3
 
     add %r4, %r1, 0xbf81
-    cmp %r4, 0xc000
-    jge out5
-    add %r4, %r4, 0x3000
-  out5:
-    cmp %r4, 0xefff
-    jle out6
-    sub %r4, %r4, 0x3000
-  out6:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; up right
     add %r2, %r2, %r3
 
     add %r4, %r1, 0xbfff
-    cmp %r4, 0xc000
-    jge out7
-    add %r4, %r4, 0x3000
-  out7:
-    cmp %r4, 0xefff
-    jle out8
-    sub %r4, %r4, 0x3000
-  out8:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; left
     add %r2, %r2, %r3
 
     add %r4, %r1, 0xc001
-    cmp %r4, 0xc000
-    jge out9
-    add %r4, %r4, 0x3000
-  out9:
-    cmp %r4, 0xefff
-    jle out10
-    sub %r4, %r4, 0x3000
-  out10:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0  ; right
     add %r2, %r2, %r3
 
     add %r4, %r1, 0xc07f
-    cmp %r4, 0xc000
-    jge out11
-    add %r4, %r4, 0x3000
-  out11:
-    cmp %r4, 0xefff
-    jle out12
-    sub %r4, %r4, 0x3000
-  out12:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; down left
     add %r2, %r2, %r3
 
     add %r4, %r1, 0xc080
-    cmp %r4, 0xc000
-    jge out13
-    add %r4, %r4, 0x3000
-  out13:
-    cmp %r4, 0xefff
-    jle out14
-    sub %r4, %r4, 0x3000
-  out14:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; down
     add %r2, %r2, %r3
 
     add %r4, %r1, 0xc081
-    cmp %r4, 0xc000
-    jge out15
-    add %r4, %r4, 0x3000
-  out15:
-    cmp %r4, 0xefff
-    jle out16
-    sub %r4, %r4, 0x3000
-  out16:
+    add %ra, %pc, 4
+    jmp wrap_r4
     lbu %r3, %r4, 0 ; down right
     add %r2, %r2, %r3
 
@@ -133,3 +85,14 @@ cycle:
     jne blit_loop
 
   jmp cycle
+
+wrap_r4:
+    cmp %r4, 0xc000
+    jge out1
+    add %r4, %r4, 0x3000
+  out1:
+    cmp %r4, 0xefff
+    jle out2
+    sub %r4, %r4, 0x3000
+  out2:
+    jmp %ra
